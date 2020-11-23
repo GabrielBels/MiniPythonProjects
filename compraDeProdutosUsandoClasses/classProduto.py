1049fa30885314c1388232e9128087ac601e08f3
Gabriel Belisario <gabriel.mhb@gmail.com>
2020-11-23 09:55:53 -0300

    Create classProduto.py

---
+++ compraDeProdutosUsandoClasses/classProduto.py
@@ -0,0 +1,16 @@
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+   
+    def setNome(self, nome):
+        self.nome = nome
+       
+    def setPreco(self, preco):
+        self.preco = preco
+       
+    def getNome():
+        return self.nome
+       
+    def getPreco():
+        return self.preco
